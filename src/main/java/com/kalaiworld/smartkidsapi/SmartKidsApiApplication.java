ab07cfe70aca8394a47a56380ec8805e95e5c1a0
kalai <29577317+kulaganathan@users.noreply.github.com>
2020-10-04 01:57:11 -0500

    initial commit

---
+++ src/main/java/com/kalaiworld/smartkidsapi/SmartKidsApiApplication.java
@@ -0,0 +1,13 @@
+package com.kalaiworld.smartkidsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartKidsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartKidsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalaiworld/smartkidsapi/SmartKidsApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kalaiworld.smartkidsapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartKidsApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
