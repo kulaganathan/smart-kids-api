57ddb76f5d501d1299a736dfc28fa730fd17fb77
kalai <29577317+kulaganathan@users.noreply.github.com>
2020-12-30 03:17:22 -0600

    Added response entity to program responses

--- smart-kids-rest-api/src/main/java/com/kalaiworld/smartkidsapi/controller/ProgramController.java
+++ smart-kids-rest-api/src/main/java/com/kalaiworld/smartkidsapi/controller/ProgramController.java
@@ -4,6 +4,8 @@ import com.kalaiworld.smartkidsapi.entity.Program;
 import com.kalaiworld.smartkidsapi.repository.ProgramRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +21,16 @@ public class ProgramController {
     private ProgramRepository programRepository;
 
     @GetMapping
-    public List<Program> getPrograms() {
+    public ResponseEntity<?> getPrograms() {
         log.info("Inside programs GET API controller");
-        log.info("Program Repo call - " + programRepository.findAll().toString());
-        return programRepository.findAll();
+        try {
+            log.info("Program Repo call - " + programRepository.findAll().toString());
+            programRepository.findAll();
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(programRepository.findAll());
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("ISE");
+        }
     }
 }
